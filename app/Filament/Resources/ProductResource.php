76f49091480d24c567975fffdb941e4450f008b1
Mikó Zoltán <mikozoltan91@gmail.com>
2024-10-22 16:25:07 +0200

    finshed the admin pages and start develop shop pages

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -24,6 +24,22 @@ class ProductResource extends Resource
     protected static ?string $navigationGroup = 'Shop';
     protected static ?int $navigationSort = 2;
 
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['name', 'categories.title'];
+    }
+
+    /*
+    public static function getGlobalSearchResultDetails(Model $record): array
+    {
+        return [
+            'Category' => $record->category->title,
+        ];
+    }
+    */
+
     public static function form(Form $form): Form
     {
         return $form
--- app/Providers/Filament/OfficePanelProvider.php
+++ app/Providers/Filament/OfficePanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
@@ -47,8 +48,9 @@ class OfficePanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                OrderStats::class
+                //Widgets\AccountWidget::class,
+                //Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
