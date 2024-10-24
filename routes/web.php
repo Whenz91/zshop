7f77170381e38f3a145244d3b229eb2a41baf3bd
Mikó Zoltán <mikozoltan91@gmail.com>
2024-10-24 16:14:10 +0200

    make some shop pages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
+use App\Livewire\ProductDetailPage;
+use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class)->name('categories');
+Route::get('/products', ProductsPage::class)->name('products');
+Route::get('/products/{product}', ProductDetailPage::class)->name('product');
+Route::get('/cart', CartPage::class)->name('cart');
