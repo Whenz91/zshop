7a8619551bf992cea592593c8a078c5fde3cd28a
Mikó Zoltán <mikozoltan91@gmail.com>
2024-11-15 14:44:10 +0100

    start working in shop auth system

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,10 +2,44 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Regisztráció - Zolárium')]
 class RegisterPage extends Component
 {
+
+    public $name;
+    public $email;
+    public $password;
+    public $password_confirmation;
+    public $accept_terms;
+
+    //registrate user
+    public function save() {
+        $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:6|max:255|confirmed|same:password_confirmation',
+            'password_confirmation' => 'required|min:6|max:255',
+            'accept_terms' => 'accepted'
+        ]);
+
+        //save to db
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password)
+        ]);
+
+        //login
+        auth()->login($user);
+
+        return redirect()->intended();
+    }
+
     public function render()
     {
         return view('livewire.auth.register-page');
--- resources/views/livewire/auth/register-page.blade.php
+++ resources/views/livewire/auth/register-page.blade.php
@@ -25,18 +25,23 @@
                 <div class="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-600 dark:after:border-neutral-600">Vagy</div>
 
                 <!-- Form -->
-                <form>
+                <form wire:submit.prevent="">
                     <div class="grid gap-y-4">
+                        <!-- Form Group -->
+                        <div>
+                            <label for="name" class="block text-sm mb-2 dark:text-white">Név</label>
+                            <div class="relative">
+                                <input type="text" id="name" wire:model="name" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" aria-describedby="name-error">
+                            </div>
+                            <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
+                        </div>
+                        <!-- End Form Group -->
+
                         <!-- Form Group -->
                         <div>
                             <label for="email" class="block text-sm mb-2 dark:text-white">Email cím</label>
                             <div class="relative">
-                                <input type="email" id="email" name="email" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="email-error">
-                                <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-                                    <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                                    </svg>
-                                </div>
+                                <input type="email" id="email" wire:model="email" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" aria-describedby="email-error">
                             </div>
                             <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
                         </div>
@@ -46,12 +51,7 @@
                         <div>
                             <label for="password" class="block text-sm mb-2 dark:text-white">Jelszó</label>
                             <div class="relative">
-                                <input type="password" id="password" name="password" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="password-error">
-                                <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-                                    <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                                    </svg>
-                                </div>
+                                <input type="password" id="password" wire:model="password" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" aria-describedby="password-error">
                             </div>
                             <p class="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
                         </div>
@@ -59,26 +59,21 @@
 
                         <!-- Form Group -->
                         <div>
-                            <label for="confirm-password" class="block text-sm mb-2 dark:text-white">Jelszó ismét</label>
+                            <label for="password_confirmation" class="block text-sm mb-2 dark:text-white">Jelszó ismét</label>
                             <div class="relative">
-                                <input type="password" id="confirm-password" name="confirm-password" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="confirm-password-error">
-                                <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-                                    <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                                    </svg>
-                                </div>
+                                <input type="password" id="password_confirmation" wire:model="password_confirmation" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" aria-describedby="password_confirmation-error">
                             </div>
-                            <p class="hidden text-xs text-red-600 mt-2" id="confirm-password-error">Password does not match the password</p>
+                            <p class="hidden text-xs text-red-600 mt-2" id="password_confirmation-error">Password does not match the password</p>
                         </div>
                         <!-- End Form Group -->
 
                         <!-- Checkbox -->
                         <div class="flex items-center">
                             <div class="flex">
-                                <input id="remember-me" name="remember-me" type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800">
+                                <input id="accept-terms" wire:model="accept_terms" type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800">
                             </div>
                             <div class="ms-3">
-                                <label for="remember-me" class="text-sm dark:text-white">Elfogadom a <a class="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500" href="#">Felhasználói feltételeket</a></label>
+                                <label for="accept-terms" class="text-sm dark:text-white">Elfogadom a <a class="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500" href="#">Felhasználói feltételeket</a></label>
                             </div>
                         </div>
                         <!-- End Checkbox -->
